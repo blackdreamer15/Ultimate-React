2baf854145833ed1cc4171232041f7d9c0bd844e
Jessy Baki <bakijessy@gmail.com>
2023-05-24 22:46:03 +0000

    Add h2 heading to form in SplitBillForm component

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ function AddFriendForm({ name, onSetName, image, onSetImage }) {
 function SplitBillForm() {
   return (
     <form className="form-split-bill">
-
+      <h2>Split a bill with X</h2>
     </form>
   );
 }
