6383d8fcb745429b9cda9cfe126185cdf80e9537
Jessy Baki <bakijessy@gmail.com>
2023-06-03 09:05:24 +0000

    Import useState hook in App.js

--- 6. tip-calculator-challenge/src/App.js
+++ 6. tip-calculator-challenge/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function App() {
     return (
         <BillInput />
@@ -5,6 +7,8 @@ export default function App() {
 }
 
 function BillInput() {
+
+
     return (
         <div>
             <label>How much was the bill?</label>
