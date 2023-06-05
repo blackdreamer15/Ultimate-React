7a12f8de56b15b23dcc59a20f76f1da37419aa7f
Jessy Baki <bakijessy@gmail.com>
2023-06-05 09:40:17 +0000

    Add tip value in content of option elements

--- 6. tip-calculator-challenge/src/App.js
+++ 6. tip-calculator-challenge/src/App.js
@@ -34,10 +34,10 @@ function SelectPercentage({ children }) {
             <label>{children}</label>
 
             <select value={percentage} onChange={e => setPercentage(Number(e.target.value))}>
-                <option value={0}>Dissatisfied ()</option>
-                <option value={5}>It was okay ()</option>
-                <option value={10}>It was good ()</option>
-                <option value={20}>Absolutely amazing! ()</option>
+                <option value={0}>Dissatisfied (0%)</option>
+                <option value={5}>It was okay (5%)</option>
+                <option value={10}>It was good (10%)</option>
+                <option value={20}>Absolutely amazing! (20%)</option>
             </select>
         </div>
     );
