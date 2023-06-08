18a9d2e6735cf15a397ae2c5aa6e11fb82a274a7
Jessy Baki <bakijessy@gmail.com>
2023-06-08 22:08:58 +0000

    Pass event handling function as prop to Reset

--- 6. tip-calculator-challenge/src/App.js
+++ 6. tip-calculator-challenge/src/App.js
@@ -35,7 +35,7 @@ function TipCalculator() {
 
             <Output bill={bill} tip1={percentage1} tip2={percentage2} />
 
-            <Reset />
+            <Reset onReset={onReset} />
         </div>
     );
 }
