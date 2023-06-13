d352e6a659ce94e7223c738984be0e34dc4088f4
Jessy Baki <bakijessy@gmail.com>
2023-06-13 11:29:49 +0000

    Add an array of objects containing skill details

--- profile-card/src/index.js
+++ profile-card/src/index.js
@@ -2,7 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-
+const skills = [
+    {
+        "skill": "HTML+CSS",
+        "level": "advanced",
+        "color": "#2662EA"
+    },
+    {
+        "skill": "SCSS",
+        "level": "advanced",
+        "color": "orangered"
+    },
+    {
+        "skill": "Bootstrap",
+        "level": "intermediate",
+        "color": "#260aaa"
+    },
+    {
+        "skill": "JavaScript",
+        "level": "advanced",
+        "color": "#EFD81D"
+    },
+    {
+        "skill": "React",
+        "level": "advanced",
+        "color": "#60DAFB"
+    },
+    {
+        "skill": "Styled Components",
+        "level": "beginner",
+        "color": "#ff00ff"
+    },
+    {
+        "skill": "Svelte",
+        "level": "beginner",
+        "color": "#FF3B00"
+    },
+    {
+        "skill": "Web Design",
+        "level": "advanced",
+        "color": "#C3DCAF"
+    },
+    {
+        "skill": "Git & GitHub",
+        "level": "intermediate",
+        "color": "#E84F33"
+    },
+    {
+        "skill": "Node.js",
+        "level": "beginner",
+        "color": "#00ff00"
+    },
+    {
+        "skill": "Express.js",
+        "level": "beginner",
+        "color": "#ff00ff"
+    },
+    {
+        "skill": "PostgreSQL",
+        "level": "beginner",
+        "color": "#a4faff"
+    },
+];
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
