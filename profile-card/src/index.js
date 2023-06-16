a64ef5791db0c603d349d9a926e3763d152fa02f
Jessy Baki <bakijessy@gmail.com>
2023-06-16 10:12:07 +0000

    Add Avatar component starter component

--- profile-card/src/index.js
+++ profile-card/src/index.js
@@ -78,6 +78,14 @@ function App() {
     );
 }
 
+function Avatar() {
+    return (
+    
+    );
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
