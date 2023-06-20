f60581c2ec30064ab6a0293cd3054ed28d25f7fa
Jessy Baki <bakijessy@gmail.com>
2023-06-20 14:23:23 +0000

    Add SkillList component to index.js

--- profile-card/src/index.js
+++ profile-card/src/index.js
@@ -93,6 +93,12 @@ function Intro() {
     );
 }
 
+function SkillList() {
+    return (
+        
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
