d8f5851953fc9dc99d0c749976902d93aec3c1b5
Jessy Baki <bakijessy@gmail.com>
2023-06-26 22:38:02 +0000

    Add className active to first div in numbers div

--- steps-component/src/App.js
+++ steps-component/src/App.js
@@ -2,7 +2,7 @@ function App() {
     return (
         <div className="steps">
             <div className="numbers">
-                <div>1</div>
+                <div className="active">1</div>
                 <div>2</div>
                 <div>3</div>
             </div>
