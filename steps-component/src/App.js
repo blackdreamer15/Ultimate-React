a3216f0c2ee69c727efd430f769d287b58d88365
Jessy Baki <bakijessy@gmail.com>
2023-06-26 23:38:34 +0000

    Add ternary operator to add active className to div

--- steps-component/src/App.js
+++ steps-component/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <div className="steps">
             <div className="numbers">
                 <div className={step >= 1 ? "active" : ""}>1</div>
-                <div>2</div>
+                <div className={step >= 2 ? "active" : ""}>2</div>
                 <div>3</div>
             </div>
 
