6b810929338a07ee1ed19f39afd0e5dea6316fe4
Jessy Baki <bakijessy@gmail.com>
2023-06-30 15:44:45 +0000

    Add onClick events to buttons

--- steps-component/src/App.js
+++ steps-component/src/App.js
@@ -28,14 +28,14 @@ function App() {
             </p>
 
             <div className="buttons">
-                <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+                <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious()}>
                     <span>Previous</span>
                 </button>
-                <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+                <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext()} >
                     <span>Next</span>
                 </button>
             </div>
-        </div>
+        </div >
     );
 }
 export default App;
