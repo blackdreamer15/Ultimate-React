411b783dcc360773b76e5ed79592e0518851dd53
Jessy Baki <bakijessy@gmail.com>
2023-07-01 19:44:08 +0000

    Wrap entire content of App component in a div

--- steps-component/src/App.js
+++ steps-component/src/App.js
@@ -18,26 +18,29 @@ function App() {
     }
 
     return (
-        <div className="steps">
-            <div className="numbers">
-                <div className={step >= 1 ? "active" : ""}>1</div>
-                <div className={step >= 2 ? "active" : ""}>2</div>
-                <div className={step >= 3 ? "active" : ""}>3</div>
-            </div>
+        <div>
+            <div className="steps">
+                <div className="numbers">
+                    <div className={step >= 1 ? "active" : ""}>1</div>
+                    <div className={step >= 2 ? "active" : ""}>2</div>
+                    <div className={step >= 3 ? "active" : ""}>3</div>
+                </div>
 
-            <p className="message">
-                {`Step ${step}: ${messages[step - 1]}`}
-            </p>
+                <p className="message">
+                    {`Step ${step}: ${messages[step - 1]}`}
+                </p>
 
-            <div className="buttons">
-                <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
-                    <span>Previous</span>
-                </button>
-                <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext} >
-                    <span>Next</span>
-                </button>
-            </div>
-        </div >
+                <div className="buttons">
+                    <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
+                        <span>Previous</span>
+                    </button>
+                    <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext} >
+                        <span>Next</span>
+                    </button>
+                </div>
+            </div >
+        </div>
+
     );
 }
 export default App;
