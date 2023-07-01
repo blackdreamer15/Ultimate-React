fd9d648f04ae6ce6ced63cc6909a6664ce7e8fec
Jessy Baki <bakijessy@gmail.com>
2023-07-01 19:50:42 +0000

    Add condition to display steps if isOpen is true

--- steps-component/src/App.js
+++ steps-component/src/App.js
@@ -22,26 +22,28 @@ function App() {
         <div>
             <button className="close">&times;</button>
 
-            <div className="steps">
-                <div className="numbers">
-                    <div className={step >= 1 ? "active" : ""}>1</div>
-                    <div className={step >= 2 ? "active" : ""}>2</div>
-                    <div className={step >= 3 ? "active" : ""}>3</div>
-                </div>
+            {isOpen &&
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
+                    </div>
 
-                <p className="message">
-                    {`Step ${step}: ${messages[step - 1]}`}
-                </p>
+                    <p className="message">
+                        {`Step ${step}: ${messages[step - 1]}`}
+                    </p>
 
-                <div className="buttons">
-                    <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
-                        <span>Previous</span>
-                    </button>
-                    <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext} >
-                        <span>Next</span>
-                    </button>
-                </div>
-            </div >
+                    <div className="buttons">
+                        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
+                            <span>Previous</span>
+                        </button>
+                        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext} >
+                            <span>Next</span>
+                        </button>
+                    </div>
+                </div >
+            }
         </div>
 
     );
