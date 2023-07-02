9c49ad1ecfc9e0b9f5b18d9af6ebeb0bfc9867df
Jessy Baki <bakijessy@gmail.com>
2023-07-02 23:44:19 +0000

    Render the root element and add App component

--- 3. date-counter/src/index.js
+++ 3. date-counter/src/index.js
@@ -1,3 +1,10 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './App'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        < App />
+    </React.StrictMode>
+)
