c358fb8adbb16c4df9a67edf58ff5170904fe39c
Jessy Baki <bakijessy@gmail.com>
2023-07-03 00:34:18 +0000

    Remove Steps component and edit Counter component

--- 3. date-counter/src/App.js
+++ 3. date-counter/src/App.js
@@ -3,14 +3,16 @@ import { useState } from "react";
 function App() {
     return (
         <div>
-            < Steps />
             < Counter />
         </div>
     )
 }
 
-function Steps() {
+
+function Counter() {
     const [step, setStep] = useState(0);
+    const [count, setCount] = useState(0);
+
 
     function handleStepIncrement() {
         setStep(step => step + 1);
@@ -20,17 +22,6 @@ function Steps() {
         setStep(step => step - 1);
     }
 
-    return (
-        <div>
-            <button onClick={handleStepDecrement}>&minus;</button>
-            <span>Step: {step}</span>
-            <button onClick={handleStepIncrement}>&plus;</button>
-        </div>
-    )
-}
-
-function Counter() {
-    const [count, setCount] = useState(0);
 
     function handleCountIncrement() {
 
@@ -41,11 +32,18 @@ function Counter() {
     }
 
     return (
-        <div>
-            <button>&minus;</button>
-            <span>Count: {count}</span>
-            <button>&plus;</button>
-        </div>
+        <>
+            <div>
+                <button>&minus;</button>
+                <span>Count: {count}</span>
+                <button>&plus;</button>
+            </div>
+            <div>
+                <button onClick={handleStepDecrement}>&minus;</button>
+                <span>Step: {step}</span>
+                <button onClick={handleStepIncrement}>&plus;</button>
+            </div>
+        </>
     )
 }
 
