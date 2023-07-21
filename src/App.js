a128c9f50e5eb1ba3cd3769390959cfb91927681
Jessy Baki <bakijessy@gmail.com>
2023-07-21 13:51:50 +0000

    Pass props to the SplitBillForm component

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ export default function App() {
         </Button>
       </div>
 
-      {showBillForm && <SplitBillForm />}
+      {showBillForm && <SplitBillForm showBillForm={showBillForm} onSetShowBillForm={handleShowBillForm} />}
     </div>
   );
 }
