14f441f30dbffe37788c143581feefba3716e231
Jessy Baki <bakijessy@gmail.com>
2023-07-22 14:27:04 +0000

    FIx typo in naming piece of state's variable

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function Button({ onClick, children }) {
 export default function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
   const [friendList, setFriendList] = useState([]);
-  const [showBillForm, setSelectedFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(false);
 
   function handleClicking() {
     setShowAddFriend(showAddFriend => !showAddFriend);
