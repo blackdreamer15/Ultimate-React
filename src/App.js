28c7b9c34a503e3e33956bd9cd744a65d806ca2c
Jessy Baki <bakijessy@gmail.com>
2023-07-26 22:55:16 +0000

    FIx: Pass item id to the onDeleteItem function

--- src/App.js
+++ src/App.js
@@ -107,7 +107,7 @@ function Item({ item, onDeleteItem }) {
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button onClick={() => onDeleteItem()}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
