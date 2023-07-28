671336280722acdffb5c31c2fd04013bf4488163
Jessy Baki <bakijessy@gmail.com>
2023-07-28 17:45:58 +0000

    Style: Add space to text in Stats component

--- src/App.js
+++ src/App.js
@@ -126,7 +126,8 @@ function Stats({ items }) {
     <footer className="stats">
       <p>
         <i>
-          {percentage === 100 ? "You got everything! Ready to go ✈️" :
+          {percentage === 100 ?
+            "You got everything! Ready to go ✈️" :
             `You have ${numOfItems} items on your list, and you have already packed ${numOfPackedItems} (${percentage}%)`}
         </i>
       </p>
