2b0884611772725d2aab19baf3bd7c83b3e0b96d
Jessy Baki <bakijessy@gmail.com>
2023-07-31 14:45:52 +0000

    Import Item component into PackingList.js

--- src/PackingList.js
+++ src/PackingList.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Item from "./Item";
 
 export default function PackingList({ items, onClearList, onDeleteItem, onUpdateItem }) {
     const [sortBy, setSortBy] = useState("input");
