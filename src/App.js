860f34f438757e0098ec905a832bdcd3d60819ec
Jessy Baki <bakijessy@gmail.com>
2023-07-31 14:44:25 +0000

    Import Stats component into App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
+import Stats from "./Stats";
 
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
