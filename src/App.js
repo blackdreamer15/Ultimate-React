32cf1a9ad742769afb874e92d3aeb99fffecd008
Jessy Baki <bakijessy@gmail.com>
2023-07-31 14:42:01 +0000

    Import PackingList component into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
+import PackingList from "./PackingList";
 
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
