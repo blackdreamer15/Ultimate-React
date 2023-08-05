4ba7087d19845ffa30df7d00a2bab24210174cbe
Jessy Baki <bakijessy@gmail.com>
2023-08-05 15:41:10 +0000

    Add Message component into App & pass in step prop

--- 2. steps-component/src/App.js
+++ 2. steps-component/src/App.js
@@ -38,9 +38,7 @@ export default function App() {
                         <div className={step >= 3 ? "active" : ""}>3</div>
                     </div>
 
-                    <p className="message">
-                        {`Step ${step}: ${messages[step - 1]}`}
-                    </p>
+                    <Message step={step} />
 
                     <div className="buttons">
                         <Button bgColor="#7950f2" color="#fff" onClick={handlePrevious}>
