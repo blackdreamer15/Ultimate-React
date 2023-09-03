653ec6f267c590632ebe5e84e8a7e1a3d6322fd7
Jessy Baki <bakijessy@gmail.com>
2023-09-03 06:00:41 +0000

    Add a select and options in that select element

--- 8. currency-converter-challenge/src/App.js
+++ 8. currency-converter-challenge/src/App.js
@@ -9,6 +9,13 @@ export default function App() {
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
+
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
     </div>
   );
 }
