4976b00558da04837c893a311062b2fca2621376
Jessy Baki <bakijessy@gmail.com>
2023-09-03 07:42:47 +0000

    Set amount to intial amount when quote equals base

--- 8. currency-converter-challenge/src/App.js
+++ 8. currency-converter-challenge/src/App.js
@@ -25,6 +25,10 @@ export default function App() {
         }
       }
 
+      if (quoteCurrency === baseCurrency) {
+        setAmount(amount);
+      }
+
       fetchConversion();
 
     }, [amount, baseCurrency, quoteCurrency]);
