640338a10392b8277fd4fe388c0616b1c02ffe40
Jessy Baki <bakijessy@gmail.com>
2023-10-14 06:26:40 +0000

    Extract some states & functions in custom provider

--- 13-atomic-blog/src/PostContext.js
+++ 13-atomic-blog/src/PostContext.js
@@ -1,8 +1,35 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { createRandomPost } from "./RandomPost";
 
 const PostContext = createContext();
 
+
+
 function PostProvider({ chidren }) {
+    const [posts, setPosts] = useState(() =>
+        Array.from({ length: 30 }, () => createRandomPost())
+    );
+    const [searchQuery, setSearchQuery] = useState("");
+
+    // Derived state. These are the posts that will actually be displayed
+    const searchedPosts =
+        searchQuery.length > 0
+            ? posts.filter((post) =>
+                `${post.title} ${post.body}`
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+            )
+            : posts;
+
+    function handleAddPost(post) {
+        setPosts((posts) => [post, ...posts]);
+    }
+
+    function handleClearPosts() {
+        setPosts([]);
+    }
+
+
     return (
         <PostContext.Provider value={{
             posts: searchedPosts,
