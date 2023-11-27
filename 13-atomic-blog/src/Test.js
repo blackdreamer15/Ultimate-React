19932836f42ed9b1d08ee7069847bcacd5684688
blackdreamer15 <bakijessy@gmail.com>
2023-11-27 23:38:02 +0000

    chore: Apply component composition to Counter component

--- 13-atomic-blog/src/Test.js
+++ 13-atomic-blog/src/Test.js
@@ -14,13 +14,13 @@ function SlowComponent() {
 	);
 }
 
-function Counter() {
+function Counter({ children }) {
 	const [count, setCount] = useState(0);
 	return (
 		<div>
 			<h1>Slow counter?!?</h1>
 			<button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-			<SlowComponent />
+			{children}
 		</div>
 	);
 }
