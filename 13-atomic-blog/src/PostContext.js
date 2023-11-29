a2dc3e644de5c350741c9413f4e7886593c5e83c
blackdreamer15 <bakijessy@gmail.com>
2023-11-29 20:55:21 +0000

    perf: Memoize values of PostContext

--- 13-atomic-blog/src/PostContext.js
+++ 13-atomic-blog/src/PostContext.js
@@ -1,61 +1,59 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
-
 function createRandomPost() {
-    return {
-        title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-        body: faker.hacker.phrase(),
-    };
+	return {
+		title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+		body: faker.hacker.phrase(),
+	};
 }
 
 const PostContext = createContext();
 
 function PostProvider({ chidren }) {
-    const [posts, setPosts] = useState(() =>
-        Array.from({ length: 30 }, () => createRandomPost())
-    );
-    const [searchQuery, setSearchQuery] = useState("");
+	const [posts, setPosts] = useState(() =>
+		Array.from({ length: 30 }, () => createRandomPost())
+	);
+	const [searchQuery, setSearchQuery] = useState("");
 
-    // Derived state. These are the posts that will actually be displayed
-    const searchedPosts =
-        searchQuery.length > 0
-            ? posts.filter((post) =>
-                `${post.title} ${post.body}`
-                    .toLowerCase()
-                    .includes(searchQuery.toLowerCase())
-            )
-            : posts;
+	// Derived state. These are the posts that will actually be displayed
+	const searchedPosts =
+		searchQuery.length > 0
+			? posts.filter((post) =>
+					`${post.title} ${post.body}`
+						.toLowerCase()
+						.includes(searchQuery.toLowerCase())
+			  )
+			: posts;
 
-    function handleAddPost(post) {
-        setPosts((posts) => [post, ...posts]);
-    }
+	function handleAddPost(post) {
+		setPosts((posts) => [post, ...posts]);
+	}
 
-    function handleClearPosts() {
-        setPosts([]);
-    }
+	function handleClearPosts() {
+		setPosts([]);
+	}
 
+	const value = useMemo(() => {
+		return {
+			posts: searchedPosts,
+			onAddPost: handleAddPost,
+			onClearPosts: handleClearPosts,
+			searchQuery,
+			setSearchQuery,
+		};
+	}, [searchQuery, searchedPosts]);
 
-    return (
-        <PostContext.Provider value={{
-            posts: searchedPosts,
-            onAddPost: handleAddPost,
-            onClearPosts: handleClearPosts,
-            searchQuery,
-            setSearchQuery,
-        }}>
-            {chidren}
-        </PostContext.Provider>
-    );
+	return <PostContext.Provider value={value}>{chidren}</PostContext.Provider>;
 }
 
 function usePosts() {
-    const context = useContext(PostContext);
+	const context = useContext(PostContext);
 
-    if (context === undefined)
-        throw new Error("PostContext was used outside of the PostProvider");
+	if (context === undefined)
+		throw new Error("PostContext was used outside of the PostProvider");
 
-    return context;
+	return context;
 }
 
 export { PostProvider, usePosts };
